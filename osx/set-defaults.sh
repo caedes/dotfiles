edd84b423e767db1c3e94086da400339937d10c9
Romain Laurent <romain.laurent@wopata.com>
2013-04-28 15:40:08 +0200

    Add update function with osx default params

---
+++ osx/set-defaults.sh
@@ -0,0 +1,5 @@
+# Always open everything in Finder's list view
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+
+# Set a really fast key repeat.
+defaults write NSGlobalDomain KeyRepeat -int 0
