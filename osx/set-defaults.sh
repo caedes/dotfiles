f385b5f0149c5afb5faf610a693c3b7c76d7d925
caedes <romain.laurent@wopata.com>
2014-03-12 09:52:53 +0100

    Add tab switching modal windows

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -1,8 +1,11 @@
-# Always open everything in Finder's list view
+echo "✓ Always open everything in Finder's list view"
 defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 
-# Set a really fast key repeat.
+echo "✓ Set a really fast key repeat"
 defaults write NSGlobalDomain KeyRepeat -int 0
 
-# Hide hidden files by default
+echo "✓ Hide hidden files by default"
 defaults write com.apple.finder AppleShowAllFiles 0
+
+echo "✓ Enable tab switching in modal windows and other keyboard navigation perks"
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
