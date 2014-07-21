e8bea9c06a3014c81bcd62bd9316d2514c472956
caedes <romain.laurent@wopata.com>
2014-07-21 17:13:53 +0200

    Disable OSX Dashboard

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -9,3 +9,7 @@ defaults write com.apple.finder AppleShowAllFiles 0
 
 echo "✓ Enable tab switching in modal windows and other keyboard navigation perks"
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+echo "✓ Disable Dashboard"
+defaults write com.apple.dashboard mcx-disabled -boolean YES
+killall Dock
