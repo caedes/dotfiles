289d296528b1ee2c0a68b53daebceeb79c1d28c7
caedes <romain.laurent@wopata.com>
2014-07-29 15:45:47 +0200

    Add docs

--- home/zsh_aliases.sh
+++ home/zsh_aliases.sh
@@ -131,6 +131,18 @@ alias migrate="rdm && rdtp"
 
 # ## RBENV
 
+# You can pass a Ruby version on first param.
+#
+# ```bash
+# $ rbenvinit 1.9.3-p448
+# ```
+#
+# You can list all Ruby version installed by:
+#
+# ```bash
+# $ rbenv versions
+# ```
+
 function rbenvinit () {
   if [ $# -eq 1 ]; then
     echo $1 > .ruby-version
