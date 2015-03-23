1a6f223822a25e228552ee73d2de77d6fbd8393b
caedes <romain.laurent@wopata.com>
2015-03-23 11:02:58 +0100

    Add powit

--- home/zsh_aliases.sh
+++ home/zsh_aliases.sh
@@ -4,6 +4,26 @@
 
 alias lod='echo -n ಠ_ಠ | LANG=en_US.UTF-8 pbcopy'
 
+function powit() {
+  if [ -z "$1" ]; then
+    echo 'Using default port 5000'
+    port=5000
+  else
+    port=$1
+  fi
+
+  if [ -z "$2" ]; then
+    echo 'Using basename as domain : '`basename $PWD`
+    domain=`basename $PWD`
+  else
+    domain=$2
+  fi
+
+  echo $port > ~/.pow/$domain
+
+  echo "0.0.0.0:$port > $domain.dev"
+}
+
 # Classico aliases
 alias gst="git status -sb"
 alias gds="git diff --staged"
