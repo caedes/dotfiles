e5f843eb212e432ae2c7d82081ba5b562584a3d5
caedes <laurentromain@gmail.com>
2015-12-02 15:03:02 +0100

    Fix OSX aliases

--- home/.aliases/osx.sh
+++ home/.aliases/osx.sh
@@ -1,2 +1,2 @@
-alias shf="defaults write com.apple.finder AppleShowAllFiles 1"
-alias hhf="defaults write com.apple.finder AppleShowAllFiles 0"
+alias shf="defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app"
+alias hhf="defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app"
