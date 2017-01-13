8141b6af5721a3bb3ec8c57448e2d66b038cbbe5
caedes <laurentromain@gmail.com>
2017-01-13 15:42:32 +0100

    feat: update keyboard config

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -2,7 +2,8 @@ echo "✓ Always open everything in Finder's list view"
 defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 
 echo "✓ Set a really fast key repeat"
-defaults write NSGlobalDomain KeyRepeat -int 0
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
 echo "✓ Hide hidden files by default"
 defaults write com.apple.finder AppleShowAllFiles 0
