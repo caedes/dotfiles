00f36a438c60a869b4ec74076fa60ab7ce4f3024
caedes <laurentromain@gmail.com>
2018-03-24 23:26:25 +0100

    feat: add some text aliasesx

--- home/.aliases/text.sh
+++ home/.aliases/text.sh
@@ -1,4 +1,6 @@
-alias lod='echo -n ಠ_ಠ | LANG=en_US.UTF-8 pbcopy'
 alias flip='echo -n "(╯°□°）╯︵ ┻━┻" | LANG=en_US.UTF-8 pbcopy'
-alias week='date +"%V"'
+alias fu='echo -n "┌∩┐(◣_◢)┌∩┐" | LANG=en_US.UTF-8 pbcopy'
+alias lod='echo -n ಠ_ಠ | LANG=en_US.UTF-8 pbcopy'
 alias lol="say -v Hysterical 'haaa haha'"
+alias week='date +"%V"'
+alias zzz='echo -n "(￣o￣) zzZZzzZZ" | LANG=en_US.UTF-8 pbcopy'
