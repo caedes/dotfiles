757b90d9e734f8c080121207cdc1fb5fb1da131a
Romain Laurent <caedes@macbook-pro.home>
2020-07-30 15:38:23 +0200

    feat: add meeting command

--- home/.aliases/editor.sh
+++ home/.aliases/editor.sh
@@ -1,1 +1,13 @@
 alias c="code ."
+
+function meeting() {
+  cd ~/Projects/cdiscount/meetings
+  TODAY="$(date +%Y-%m-%d)"
+  if [ -z "$1" ]; then
+    echo "# $TODAY" > "$TODAY.md"
+    code . -g "$TODAY.md"
+  else
+    echo "# $TODAY $1" > "$TODAY-$1.md"
+    code . -g "$TODAY-$1.md"
+  fi
+}
