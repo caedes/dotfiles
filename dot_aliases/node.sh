62ece5f2d4fe8d916736da76c43846325c8b0fcc
caedes <laurentromain@gmail.com>
2023-05-19 10:58:40 +0200

    feat: 🎸 remove npm ow aliases in favor of NPM plugin

--- dot_aliases/node.sh
+++ dot_aliases/node.sh
@@ -1,9 +1,4 @@
 alias ems="ember server"
-alias n="npm"
-alias ni="npm install"
-alias nr="npm run"
-alias ns="npm start"
-alias nt="npm test"
 
 function share_folder() {
   npx serve "$1" & npx ngrok http 5000
